5944fe022ec59f6f0bca3eb8bee95ea647ef9246
tony2037 <ztex030640417@gmail.com>
2018-07-17 22:19:59 +0800

    Adjust the class Motor structure

--- sketch_jul17a/sketch_jul17a.ino
+++ sketch_jul17a/sketch_jul17a.ino
@@ -19,6 +19,23 @@ int M_PWM = 9;
 int M_DIR = 7;
 
 class Motor{
+  private:
+    int MotorDirection(int DIR){
+	//params:
+	//DIR : The direction of the motor using digital mode
+	  if(DIR)
+	    digitalWrite(this->DIR, HIGH);
+	  else
+	    digitalWrite(this->DIR, LOW);
+
+	  return 0;
+	};
+
+    int MotorPWM(int PWM){
+	//params:
+	//PWM : The direction of the motor using analog mode
+	  analogWrite(this->PWM, PWM);
+	};
   public:
     int PWM;
     int DIR;
@@ -28,76 +45,61 @@ class Motor{
       this->DIR = DIR;
       this->rate = 0;
       };
+
+    int SpeedUp(){
+	  if(this->rate >= 255)
+	    Serial.println("The motor is full load");
+	  else
+	  {
+	    ++ this->rate;
+	    this->rate = this->rate << 1;
+	    -- this->rate;
+	    if(this->rate >= 255)
+	      this->rate = 255;
+	    }
+        };
+
+     int SpeedDown(){
+	  if(this->rate <= 0)
+	    Serial.println("The motor is off");
+	  else
+	  {
+	    ++ this->rate;
+	    this->rate = this->rate >> 1;
+	    -- this->rate;
+	    if(this->rate <=  0)
+	      this->rate = 0;
+	    }
+	  return 0;
+	  };
+
+    int ShutDown(){
+	MotorPWM(0);
+	return 0;
+        };
   };
 
 Motor Left = Motor(L_PWM, L_DIR);
 Motor Right = Motor(R_PWM, R_DIR);
 Motor Middle = Motor(M_PWM, M_DIR);
-int MotorDirection(Motor m, int DIR){
-/*
-params:
-DIR : The direction of the motor using digital mode
-m : The motor choosing
-*/  
-  if(DIR)
-    digitalWrite(m.DIR, HIGH);
-  else
-    digitalWrite(m.DIR, LOW);
 
-  return 0;
-}
 
-int MotorPWM(Motor m, int PWM){
-/*
-params:
-PWM : The direction of the motor using analog mode
-m : The motor choosing
-*/
-  analogWrite(m.PWM, PWM);
-}
-
-int SpeedUp(Motor *m){
-  if(m->rate >= 255)
-    Serial.println("The motor is full load");
-  else
-  {
-    ++ m->rate;
-    m->rate = m->rate << 1;
-    -- m->rate;
-    if(m->rate >= 255)
-      m->rate = 255;
-    }
-  }
-
-int SpeedDown(Motor *m){
-  if(m->rate <= 0)
-    Serial.println("The motor is off");
-  else
-  {
-    ++ m->rate;
-    m->rate = m->rate >> 1;
-    -- m->rate;
-    if(m->rate <=  0)
-      m->rate = 0;
-    }
-  return 0;
-  }
 
 int Left(){
   Serial.println("Turning Left");
   // Slow down the left motor
-  MotorPWM(Left, 0);
+  Left.ShutDown();
   // Strength the right motor
-  SpeedUp(&Right);
+  Right.SpeedUp();
   return 0;
 }
 
 int Right(){
   Serial.println("Turning Right");
   // Slow down the right motor
-  MotorPWM(Right, 0);
+  Right.ShutDown();
   // Strength the left motor
-  SpeedUp(&Left);
+  Left.SpeedUp();
   return 0;
 }
 
@@ -106,8 +108,6 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);   
   Serial.println(Left.PWM);
-  MotorDirection(Left, 1);
-  MotorPWM(Left, 255);
 }
 
 
