1dbe7c4f67f0be2fcfd54560e8376f36424ec0f9
Ztex <ztex030640417@gmail.com>
2018-07-18 13:54:25 +0800

    Have tested the UpWard();

--- sketch_jul17a/sketch_jul17a.ino
+++ sketch_jul17a/sketch_jul17a.ino
@@ -108,6 +108,14 @@ class Motor{
     MotorDirection(this->direct);
     return 0;
     };
+
+    int SetLevel(int level){
+      // level range {0, 8}
+      if(level <0 || level > 8)
+        return -1;
+
+      MotorPWM(pow(2, level) - 1);
+      };
   
 };
 
@@ -149,12 +157,20 @@ int RightWard(){
   return 0;
 };
 
+int UpWard(int level){
+  Middle.SetLevel(level);
+  return 0;  
+};
+
+int DownWard(){
+  // 7/18 2018 We decided not to do this part
+  return 0;  
+};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  for(int i = 0; i < 5; ++i)
-    RightWard();
+  UpWard(7);
 }
 
 
