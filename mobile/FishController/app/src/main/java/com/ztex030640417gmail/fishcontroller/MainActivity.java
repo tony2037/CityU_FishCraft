54d18ea130fc3f92436980ba8adfc90684605d9f
Ztex <ztex030640417@gmail.com>
2018-07-24 11:59:36 +0800

    Done adapter of BT

--- mobile/FishController/app/src/main/java/com/ztex030640417gmail/fishcontroller/MainActivity.java
+++ mobile/FishController/app/src/main/java/com/ztex030640417gmail/fishcontroller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ztex030640417gmail.fishcontroller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,18 +11,47 @@ import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import java.util.Set;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private int data = 0b00000000;
+    private BluetoothAdapter myBluetooth = null;
+    private Set pairedDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("onCreate success");
+
+
+        // BLE SUPPORTED checking
+        IfSupport();
+        // Set up BLE Adapter
+        SetUpBLE();
+
+        // Objects about BLE
+        Button Scan = (Button) findViewById(R.id.Scan);
+        Button Discon = (Button) findViewById(R.id.Disconnect);
+        ListView devicelist = (ListView)findViewById(R.id.listView);
 
         // Direction Buttons object
         Button ForWard = (Button) findViewById(R.id.FORWARD); // Get the button object
@@ -202,5 +232,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public void IfSupport(){
+        Context context = this;
+        PackageManager packageManager = context.getPackageManager();
+        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            Toast.makeText(this, "BLE NOT SUPPORTED", Toast.LENGTH_SHORT).show();
+            System.out.println("BLE NOT SUPPORTED");
+            finish();
+        }
+    }
+
+    public void SetUpBLE(){
+        final BluetoothManager BTManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter myBluetooth = BTManager.getAdapter();
+        final int REQUEST_ENABLE_BT = 1;
+        if(myBluetooth == null || !myBluetooth.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            System.out.println("Adapter is not enabled");
+        }
+    }
+
 
 }
